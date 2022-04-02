27589c727f165b3b3f98224781e14684ccca88db
Leka <Leka@DESKTOP-T7H711T>
2022-04-02 18:53:12 +0200

    First Commit
    Upload

---
+++ HumanResource/src/main/java/com/hr/app/rest/CandidateErrorResponse.java
@@ -0,0 +1,37 @@
+package com.hr.app.rest;
+
+public class CandidateErrorResponse {
+
+	public CandidateErrorResponse() {
+		// TODO Auto-generated constructor stub
+	}
+
+	private int status;
+	private String message;
+	private long timeStamp;
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public long getTimeStamp() {
+		return timeStamp;
+	}
+	public void setTimeStamp(long timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+	public CandidateErrorResponse(int status, String message, long timeStamp) {
+		this.status = status;
+		this.message = message;
+		this.timeStamp = timeStamp;
+	}
+	
+	
+}
---
+++ HumanResource/src/main/webapp/index.jsp
@@ -0,0 +1,12 @@
+<html>
+<body>
+
+<h3> Candidates </h3>
+
+<hr>
+
+<a href="${pageContext.request.contextPath}/api/candidates">Get All Candidates</a>
+
+
+</body>
+</html>
